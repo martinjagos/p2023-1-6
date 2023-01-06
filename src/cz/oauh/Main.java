9b0d98dc687df8bb01e3f8522a08b29dd51abd26
martinjagos <jagos.martin@email.cz>
2023-01-06 12:39:19 +0100

    added: Hello GitHub!

--- src/cz/oauh/Main.java
+++ src/cz/oauh/Main.java
@@ -2,6 +2,6 @@ package cz.oauh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello GitHub!");
     }
 }
